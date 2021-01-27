03b7691bae1c33b0d60ec3bbc6a48531010324b4
Dan McElroy <daniel@mcelroys.net>
2021-01-27 10:52:30 +0100

    End of 57. Browser HTTP Requests with Fetch.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log('Client side js file loaded')
+
+fetch('/weather?address=Glasgow').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error)
+        }
+        console.log(data)
+    })
+})
