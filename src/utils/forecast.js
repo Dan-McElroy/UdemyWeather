33c9296872c3df830c933d42833fbd21876beb03
Dan McElroy <daniel@mcelroys.net>
2021-01-27 12:13:39 +0100

    Extended forecast to include humidity.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -17,8 +17,8 @@ const forecast = (latitude, longitude, callback) => {
             return callback('Unable to find location')
         }
     
-        const { weather_descriptions, temperature, feelslike } = body.current
-        callback(undefined, `${weather_descriptions[0]}: it is currently ${temperature} degrees. It feels like ${feelslike} degrees out.`)
+        const { weather_descriptions, temperature, feelslike, humidity } = body.current
+        callback(undefined, `${weather_descriptions[0]}. It is currently ${temperature} degrees. It feels like ${feelslike} degrees out. The humidity is ${humidity}%.`)
     })
 }
 
