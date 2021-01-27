3c3bda77bcf91e08e9c2f08376e069bd31403ce0
Dan McElroy <daniel@mcelroys.net>
2021-01-27 11:01:10 +0100

    End of 58. Creating a Search Form

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,19 @@
 console.log('Client side js file loaded')
 
-fetch('/weather?address=Glasgow').then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(data.error)
-        }
-        console.log(data)
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const {value: location} = search
+
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                return console.log(data.error)
+            }
+            console.log(data)
+        })
     })
 })
