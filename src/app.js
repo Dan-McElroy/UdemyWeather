9c17e57ed858808250b24b52ddf08c1330d2d2ee
Dan McElroy <daniel@mcelroys.net>
2021-01-27 09:34:46 +0100

    End of 49. Advanced Templating.

--- src/app.js
+++ src/app.js
@@ -6,11 +6,13 @@ const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup hbs engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -31,8 +33,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        title: 'Help Page',
-        helpText: 'Some helpful text'
+        title: 'Help',
+        helpText: 'Some helpful text',
+        name: 'Dan McElroy'
     })
 })
 
